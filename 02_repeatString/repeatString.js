57147915620074f9041eae7bc7a77035214983e2
Subhan <subhan.tinwala@gmail.com>
2024-04-01 05:20:32 +0500

    Commit modified files so that they can be pushed to GitHub

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (stringToRepeat, n) {
+  if (n < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += stringToRepeat;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (reverse) {
+  const tempArr = reverse.split("");
+  let revString = "";
+  for (i = tempArr.length - 1; i > -1; i--) {
+    revString = revString + tempArr[i];
+  }
+  return revString;
 };
 
 // Do not edit below this line
