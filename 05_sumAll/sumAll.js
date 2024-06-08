63063cca73ab405e58d1ffca5f26713e95f04987
Subhan <subhan.tinwala@gmail.com>
2024-06-08 18:52:08 +0500

    Write function sumAll to add all numbers between 2 values inclusive

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  } else if (a <= 0 || b <= 0) {
+    return "ERROR";
+  } else if (a > b) {
+    sum = sumNumbers(b, a);
+    return sum;
+  } else {
+    sum = sumNumbers(a, b);
+    return sum;
+  }
+  function sumNumbers(a, b) {
+    let sum = a;
+    while (a < b) {
+      a++;
+      sum = sum + a;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
